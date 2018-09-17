117a0abad8c7163a51f084e2969b362fa1a7b05c
Aravindhan Elayakumar <aravindhansai@gmail.com>
2018-09-17 12:27:21 -0700

    Adding css and the final app

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Option = (props) => {
-    return <div>
-        Option: {props.optionText}
+    return <div className="option">
+        <p className="option__text">{props.count}. {props.optionText}</p>
         <button
+            className="button button--link"
             onClick={(e) => {
             props.handleDeleteOption(props.optionText)
         }}>
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,9 @@ module.exports = {
                 loader: 'babel-loader',
                 test: /\.js$/,
                 exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: ['style-loader', 'css-loader','sass-loader']
             }
         ]
     },
